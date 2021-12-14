ce1fbddfdfb643cae59352aa7d248614512d3e1f
danmainah <danielmainawaweru@gmail.com>
2021-12-14 12:09:14 +0300

    Add navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,21 @@
 const Navbar =  () => {
     return ( 
-        <h3>Navbar area</h3>
+        <nav class="navbar sticky-top navbar-light bg-light">
+          <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+            <ul class="navbar-nav d-flex justify-content-center">
+             <li class="nav-item">
+             <a class="nav-link active" aria-current="page" href="#">About</a>
+             </li>
+            <li class="nav-item">
+             <a class="nav-link" href="#">Projects</a>
+             </li>
+            <li class="nav-item">
+             <a class="nav-link" href="#">Contact</a>
+         </li>
+        </ul>
+          </div>
+        </nav>
      );
 }
 
