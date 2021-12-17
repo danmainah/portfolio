c970e791cf8c61d0105ac7f1eced30e0db1ebbbe
danmainah <danielmainawaweru@gmail.com>
2021-12-17 21:44:52 +0300

    Add footer section

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,16 @@
 const Footer = function () {
   return (
-    <h2 className="sticky-bottom" id="Contact">Footer area</h2>
+    <div className="links d-flex justify-content-center">
+      <a className="mx-3 link-info" href="https://github.com/danmainah">
+        <i className="fa fa-github fa-2x" />
+      </a>
+      <a className="mx-3 link-info" href="https://twitter.com/danmainah">
+        <i className="fa fa-twitter fa-2x" />
+      </a>
+      <a className="mx-3 link-info" href="http://www.linkedin.com/in/daniel-maina-315a38191">
+        <i className="fa fa-linkedin fa-2x" />
+      </a>
+    </div>
   );
 };
 
