9cab2de562317e77b7f75ef15229e6d6a92893e8
danmainah <danielmainawaweru@gmail.com>
2021-12-17 22:56:48 +0300

    Fixed form submission

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,41 +1,70 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
 const Contact = function () {
+  const [serverState, setServerState] = useState({
+    submitting: false,
+    status: null,
+  });
+  const handleServerResponse = (ok, msg, form) => {
+    setServerState({
+      submitting: false,
+      status: { ok, msg },
+    });
+    if (ok) {
+      form.reset();
+    }
+  };
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    setServerState({ submitting: true });
+    axios({
+      method: 'post',
+      url: 'https://formspree.io/f/xqknzjpd',
+      data: new FormData(form),
+    })
+      .then(() => {
+        handleServerResponse(true, 'Thanks! Message received', form);
+      })
+      .catch((r) => {
+        handleServerResponse(false, r.response.data.error, form);
+      });
+  };
   return (
-    <div className="row">
-      <div className="col-12 d-flex justify-content-center">
-        <form
-          action="https://formspree.io/f/xqknzjpd"
-          method="POST"
-          id="tmContactForm"
-          className="tm-bg-white-transparent"
-        >
-          <div className="form-group">
+    <div className="row d-flex justify-content-center m-3">
+      <div className="col-5">
+        <form className="tm-bg-white-transparent" onSubmit={handleOnSubmit}>
+          <div className="form-group p-3">
             <input
               type="email"
-              id="contact_email"
-              name="_replyto"
+              id="email"
+              name="email"
               className="form-control rounded-0 border-top-0 border-right-0 border-left-0"
               placeholder="Email"
               required
             />
           </div>
-          <div className="form-group">
+          <div className="form-group p-3">
             <textarea
               rows="4"
-              id="contact_message"
+              id="message"
               name="message"
               className="form-control rounded-0 border-top-0 border-right-0 border-left-0"
               placeholder="Message"
               required
             />
           </div>
-          <div className="text-center">
-            <button
-              type="submit"
-              className="btn tm-btn-submit rounded-0 text-white btn-info"
-            >
+          <div className="text-center ms-5">
+            <button className="btn ms-4 mb-2 tm-btn-submit rounded-0 text-white btn-info" type="submit" disabled={serverState.submitting}>
               Submit
             </button>
           </div>
+          {serverState.status && (
+          <p className={!serverState.status.ok ? 'errorMsg' : ''}>
+            {serverState.status.msg}
+          </p>
+          )}
         </form>
       </div>
     </div>
